c16d78d67801ac25668129a5229802866c317701
Grégoire Paulay <gregoirepaulay.pro@gmail;com>
2023-10-30 14:23:22 +0100

    Page TypeDetails done

--- src/page/TypeDetails.tsx
+++ src/page/TypeDetails.tsx
@@ -79,133 +79,167 @@ const PokeType = (): JSX.Element => {
 
   return (
     <div className="container">
-      <h1 className={data?.name}>{Upperfirst(data?.name)}</h1>
+      <div className="typeDetails">
+        <h1 className={data?.name}>{Upperfirst(data?.name)}</h1>
 
-      <div>
-        <section className="offensive">
-          <h2>Offensive properties of {data?.name}-types moves</h2>
-
+        <div>
+          <h2>Battle properties</h2>
           <div>
-            <h3>Super effective (x 2)</h3>
-            <div>
-              {data?.damage_relations.double_damage_to.map((type) => {
-                const typeId = type.url.split("/")[6];
-                return (
-                  <p
-                    key={type.name}
-                    className={type.name}
-                    onClick={() => {
-                      navigate("/PokeType/" + typeId);
-                    }}
-                  >
-                    {Upperfirst(type.name)}
-                  </p>
-                );
-              })}
-            </div>
+            <section className="properties">
+              <h2>Offensive properties of {data?.name}-types moves</h2>
+              <div>
+                <div>
+                  <h4>Super effective (x 2)</h4>
+                  <div className="effect">
+                    {data?.damage_relations.double_damage_to.map((type) => {
+                      const typeId = type.url.split("/")[6];
+                      return (
+                        <p
+                          key={type.name}
+                          className={type.name}
+                          onClick={() => {
+                            navigate("/PokeType/" + typeId);
+                          }}
+                        >
+                          {Upperfirst(type.name)}
+                        </p>
+                      );
+                    })}
+                  </div>
+                </div>
+
+                <div>
+                  <h4>Not very effective (x 0.5)</h4>
+                  <div className="effect">
+                    {data?.damage_relations.half_damage_to.map((type) => {
+                      const typeId = type.url.split("/")[6];
+                      return (
+                        <p
+                          key={type.name}
+                          className={type.name}
+                          onClick={() => {
+                            navigate("/PokeType/" + typeId);
+                          }}
+                        >
+                          {Upperfirst(type.name)}
+                        </p>
+                      );
+                    })}
+                  </div>
+                </div>
+
+                <div>
+                  <h4>No effect (x 0)</h4>
+                  <div className="effect">
+                    {data?.damage_relations.no_damage_to.map((type) => {
+                      const typeId = type.url.split("/")[6];
+                      return (
+                        <p
+                          key={type.name}
+                          className={type.name}
+                          onClick={() => {
+                            navigate("/PokeType/" + typeId);
+                          }}
+                        >
+                          {Upperfirst(type.name)}
+                        </p>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            </section>
+
+            <section className="properties">
+              <h2>Defensive properties of {data?.name}-types Pokémon</h2>
+              <div>
+                <div>
+                  <h4>Weak to (x 2)</h4>
+                  <div className="effect">
+                    {data?.damage_relations.double_damage_from.map((type) => {
+                      const typeId = type.url.split("/")[6];
+                      return (
+                        <p
+                          key={type.name}
+                          className={type.name}
+                          onClick={() => {
+                            navigate("/PokeType/" + typeId);
+                          }}
+                        >
+                          {Upperfirst(type.name)}
+                        </p>
+                      );
+                    })}
+                  </div>
+                </div>
+
+                <div>
+                  <h4>Resists (x 0.5)</h4>
+                  <div className="effect">
+                    {data?.damage_relations.half_damage_from.map((type) => {
+                      const typeId = type.url.split("/")[6];
+                      return (
+                        <p
+                          key={type.name}
+                          className={type.name}
+                          onClick={() => {
+                            navigate("/PokeType/" + typeId);
+                          }}
+                        >
+                          {Upperfirst(type.name)}
+                        </p>
+                      );
+                    })}
+                  </div>
+                </div>
+
+                <div>
+                  <h4>Immune to (x 0)</h4>
+                  <div className="effect">
+                    {data?.damage_relations.no_damage_from.map((type) => {
+                      const typeId = type.url.split("/")[6];
+                      return (
+                        <p
+                          key={type.name}
+                          className={type.name}
+                          onClick={() => {
+                            navigate("/PokeType/" + typeId);
+                          }}
+                        >
+                          {Upperfirst(type.name)}
+                        </p>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            </section>
           </div>
+        </div>
 
+        <div className="pokeList">
+          <h2>List of {data?.name} type pokémon</h2>
           <div>
-            <h3>Not very effective (x 0.5)</h3>
-            <div>
-              {data?.damage_relations.half_damage_to.map((type) => {
-                const typeId = type.url.split("/")[6];
-                return (
-                  <p
-                    key={type.name}
-                    className={type.name}
-                    onClick={() => {
-                      navigate("/PokeType/" + typeId);
-                    }}
-                  >
-                    {Upperfirst(type.name)}
-                  </p>
-                );
-              })}
-            </div>
-          </div>
-
-          <div>
-            <h3>No effect (x 0)</h3>
-            <div>
-              {data?.damage_relations.no_damage_to.map((type) => {
-                const typeId = type.url.split("/")[6];
-                return (
-                  <p
-                    key={type.name}
-                    className={type.name}
-                    onClick={() => {
-                      navigate("/PokeType/" + typeId);
-                    }}
-                  >
-                    {Upperfirst(type.name)}
-                  </p>
-                );
-              })}
-            </div>
-          </div>
-        </section>
-
-        <section className="defensive">
-          <h2>Defensive properties of {data?.name}-types Pokémon</h2>
-          <div>
-            <h3>Weak to (x 2)</h3>
-            <div>
-              {data?.damage_relations.double_damage_from.map((type) => {
-                const typeId = type.url.split("/")[6];
-                return (
-                  <p
-                    key={type.name}
-                    className={type.name}
-                    onClick={() => {
-                      navigate("/PokeType/" + typeId);
-                    }}
-                  >
-                    {Upperfirst(type.name)}
-                  </p>
-                );
-              })}
-            </div>
-          </div>
-
-          <div>
-            <h3>Resists (x 0.5)</h3>
-            <div>
-              {data?.damage_relations.half_damage_from.map((type) => {
-                const typeId = type.url.split("/")[6];
-                return (
-                  <p
-                    key={type.name}
-                    className={type.name}
-                    onClick={() => {
-                      navigate("/PokeType/" + typeId);
-                    }}
-                  >
-                    {Upperfirst(type.name)}
-                  </p>
-                );
-              })}
-            </div>
-          </div>
-
-          <div>
-            <h3>Immune to (x 0)</h3>
-            {data?.damage_relations.no_damage_from.map((type) => {
-              const typeId = type.url.split("/")[6];
+            {data?.pokemon.map((pokemon) => {
+              const url = pokemon.pokemon.url.split("/")[6];
               return (
-                <p
-                  key={type.name}
-                  className={type.name}
+                <button
+                  key={pokemon.pokemon.name}
+                  className="pokeButton"
                   onClick={() => {
-                    navigate("/PokeType/" + typeId);
+                    navigate("/pokemon/" + pokemon.pokemon.name);
                   }}
                 >
-                  {Upperfirst(type.name)}
-                </p>
+                  <p>{Upperfirst(pokemon.pokemon.name)}</p>
+                  <img
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${url}.png`}
+                    alt="Sprites pokémon"
+                  />
+                </button>
               );
             })}
           </div>
-        </section>
+        </div>
       </div>
     </div>
   );
